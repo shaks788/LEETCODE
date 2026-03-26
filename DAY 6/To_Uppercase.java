09f42eea367882734310efcc416458a4bdff00a1
shaks788 <shakthikeyan@gmail.com>
2026-03-26 08:16:27 +0530

    Day 6

---
+++ DAY 6/To_Uppercase.java
@@ -0,0 +1,10 @@
+public class To_Uppercase {
+    public static void main(String[] args){
+        Stirng str="hEllO wOrld";
+        char[] s=str.toCharArray();
+        for(int i=0;i<s.length;i++){
+            s[i]=(char)s[i-32];
+        }
+        System.out.println(new String(s));
+    }
+}
